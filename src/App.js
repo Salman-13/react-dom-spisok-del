0232550affc9f4118226b5ea53f1dbe9c541a165
Akhmadov <akhmadov.sw@gmail.com>
2021-08-21 02:03:39 +0300

    Сделал без Мансура

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from './header/Header';
+import Form from './Form';
+import Todos from './componentTodos/Todos';
+
 
 function App() {
+
+
+  const [todos, setTodos] = useState([
+    {
+      text: "Купить бананы",
+      favorite: false
+    },
+    {
+      text: "Продать квартиру",
+      favorite: false
+    },
+    {
+      text: "Начинать зарабатывать",
+      favorite: true
+    }
+    ]
+  )
+
+  const deleteTodo = (indexTodo) => {
+    const filtered =  todos.filter((todo, index) => {
+      if (index === indexTodo) {
+        return false
+      }
+      return true
+    })
+
+    setTodos(filtered)
+  }
+
+  const makeFavorite = (indexTodo) => {
+    const newTodos = todos.map((todo, index) => {
+      if (indexTodo === index){
+        return {
+          ...todo,
+          favorite: !todo.favorite
+        }
+      }
+
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const [text, setText] = useState('');
+  const addTodo = () => {
+    setTodos([ {
+      text: text,
+      favorite: false
+    },
+    ...todos]);
+
+    setText('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Form text={text} setText={setText} addTodo={addTodo}/>
+      <Todos todos={todos} makeFavorite={makeFavorite} deleteTodo={deleteTodo}/>
     </div>
   );
 }
